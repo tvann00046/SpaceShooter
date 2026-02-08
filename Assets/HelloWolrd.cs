acb8fd78a333eea0b3ec3633a2a3d36d34187375
Dinh Thanh Van <van123@gmail.com>
2026-02-08 22:12:46 +0700

    Demo4

--- Assets/HelloWolrd.cs
+++ Assets/HelloWolrd.cs
@@ -5,7 +5,7 @@ public class ShowLog : MonoBehaviour
 // Start is called before the first frame update
 void Start()
     {
-        Debug.Log("Hello World!");
+        Debug.Log("Hello World!!");
     }
 // Update is called once per frame
 void Update()
